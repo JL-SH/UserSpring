08c2ac5e4f7a5f0d3d856802ca1f95693dc66fe0
JL-SH <joseluisalado@gmail.com>
2025-06-02 17:29:25 +0200

    First steps, project structure created, initial setup, and first class created

---
+++ src/test/java/org/jslh/userspring/UserSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jslh.userspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
