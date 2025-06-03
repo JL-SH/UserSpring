9aabbc06a94282a54b83c0a6388099ede6dc75f9
JL-SH <joseluisalado@gmail.com>
2025-06-03 11:13:48 +0200

    Repository layer created, README created

---
+++ src/main/java/org/jslh/userspring/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.jslh.userspring.user.repository;
+
+import org.jslh.userspring.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
